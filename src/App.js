c2e584688ebabf9ebc82e8395dcf5ad47923c9a8
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-02-07 20:52:55 +0100

    add: initial react code

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const App = () => {
+	return (
+		<div className='categories-container'>
+			<div className='category-container'>
+				{/* <img /> */}
+				<div className='category-body-container'>
+					<h2>Violins</h2>
+					<p>Shop Now</p>
+				</div>
+				<div className='category-body-container'>
+					<h2>Violas</h2>
+					<p>Shop Now</p>
+				</div>
+				<div className='category-body-container'>
+					<h2>Cellos</h2>
+					<p>Shop Now</p>
+				</div>
+				<div className='category-body-container'>
+					<h2>Double Basses</h2>
+					<p>Shop Now</p>
+				</div>
+				<div className='category-body-container'>
+					<h2>Accessories</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
