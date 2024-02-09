4b3e412fb01a3f68690b5f157d5faaf90efa0938
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-02-09 21:21:32 +0100

    add: react-router-dom and home page component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import violinBackground from './assets/violin-bg.jpg';
-import violaBackground from './assets/viola-bg.jpg';
-import celloBackground from './assets/cello-bg.jpg';
-import doubleBassBackground from './assets/double-bass-bg.jpg';
-import accessoriesBackground from './assets/accessories-bg.jpg';
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home.component';
 
 const App = () => {
-	const categories = [
-		{ id: 1, title: 'Violins', image: violinBackground },
-		{ id: 2, title: 'Violas', image: violaBackground },
-		{ id: 3, title: 'Cellos', image: celloBackground },
-		{ id: 4, title: 'Double Basses', image: doubleBassBackground },
-		{ id: 5, title: 'Accessories', image: accessoriesBackground },
-	];
-
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<HomePage />} />
+		</Routes>
+	);
 };
 
 export default App;
---
+++ src/pages/home.component.jsx
@@ -0,0 +1,20 @@
+import violinBackground from '../assets/violin-bg.jpg';
+import violaBackground from '../assets/viola-bg.jpg';
+import celloBackground from '../assets/cello-bg.jpg';
+import doubleBassBackground from '../assets/double-bass-bg.jpg';
+import accessoriesBackground from '../assets/accessories-bg.jpg';
+import Directory from '../components/directory/directory.component';
+
+const HomePage = () => {
+	const categories = [
+		{ id: 1, title: 'Violins', image: violinBackground },
+		{ id: 2, title: 'Violas', image: violaBackground },
+		{ id: 3, title: 'Cellos', image: celloBackground },
+		{ id: 4, title: 'Double Basses', image: doubleBassBackground },
+		{ id: 5, title: 'Accessories', image: accessoriesBackground },
+	];
+
+	return <Directory categories={categories} />;
+};
+
+export default HomePage;
