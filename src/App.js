c9df2272269a37456c3407032d0027f35275eeb7
MM-WebDeveloper <milan.varnamo@gmail.com>
2024-02-10 21:11:27 +0100

    add: 404 page not found component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home.component';
 import NavigationBar from './components/navigation-bar/navigation-bar.component';
+import PageNotFound from './pages/404.component';
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<NavigationBar />}>
 				<Route index element={<HomePage />} />
+				<Route path='/*' element={<PageNotFound />} />
 			</Route>
 		</Routes>
 	);
---
+++ src/pages/404.component.jsx
@@ -0,0 +1,12 @@
+import './404.styles.scss';
+
+const PageNotFound = () => {
+	return (
+		<div className='page-not-found'>
+			<h1>404</h1>
+			<h2>Page Not Found</h2>
+		</div>
+	);
+};
+
+export default PageNotFound;
